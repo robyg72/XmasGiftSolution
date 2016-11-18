a2a682df4a409615f4bb015246370eb136cb985b
garar0499 <garar0499@SVIL01>
2016-11-18 16:49:40 +0100

    Aggiunta MainPage

--- XmasGift/XmasGift/App.cs
+++ XmasGift/XmasGift/App.cs
@@ -12,19 +12,7 @@ namespace XmasGift
         public App()
         {
             // The root page of your application
-            MainPage = new ContentPage
-            {
-                Content = new StackLayout
-                {
-                    VerticalOptions = LayoutOptions.Center,
-                    Children = {
-                        new Label {
-                            HorizontalTextAlignment = TextAlignment.Center,
-                            Text = "Welcome to Xamarin Forms!"
-                        }
-                    }
-                }
-            };
+            MainPage = new XmasMainPage();
         }
 
         protected override void OnStart()
